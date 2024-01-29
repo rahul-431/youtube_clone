747ad72a6e695f136bff75e0865ae74a8dada395
Rahul Mijar <rahul.mijar@trunary.com>
2024-01-29 23:52:31 +0545

    feat: file uploading feature in cloudinary

---
+++ src/utils/fileUpload.js
@@ -0,0 +1,26 @@
+import { v2 as cloudinary } from "cloudinary";
+import fs from "fs";
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUINARY_API_KEY,
+  api_secret: process.env.CLOUINARY_API_SECRET,
+});
+const upload = async (localFilePath) => {
+  try {
+    if (!localFilePath) {
+      throw new Error("can not find the path");
+    }
+    //uploading file in cloudinary
+    const response = await cloudinary.uploader.upload(localFilePath, {
+      resource_type: "auto",
+    });
+    //file is uploaded successfully
+    console.log("file is uploaded successfully! at ", response.url);
+    return response;
+  } catch (error) {
+    //for remove the locally saved temporary file as the upload operation is failed
+    fs.unlinkSync(localFilePath);
+    return null;
+  }
+};
+export { upload };
